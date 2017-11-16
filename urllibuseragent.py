a8ebc64938c2d751d865b5e733cf23194b730b0b
赵中豪 <33300572+zhaozhongh@users.noreply.github.com>
2017-11-16 14:22:14 +0800

    Create urllibuseragent.py

---
+++ urllibuseragent.py
@@ -0,0 +1,11 @@
+import urllib  
+import urllib2  
+ 
+url = 'https://www.youtube.com/'
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'  
+values = {'username' : 'cht980923@gmail.com',  'password' : 'XXXX' }  
+headers = { 'User-Agent' : user_agent }  
+data = urllib.urlencode(values)  
+request = urllib2.Request(url, data, headers)  
+response = urllib2.urlopen(request)  
+page = response.read()

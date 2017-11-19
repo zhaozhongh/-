65b11fb0c47d56dc4329b4a0615008a6eb2ab1b0
赵中豪 <33300572+zhaozhongh@users.noreply.github.com>
2017-11-19 07:35:15 +0000

    Delete HELLOKITTY.py

--- HELLOKITTY.py
+++
@@ -1,7 +0,0 @@
-Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> y = 'Hello Kitty Hello Hello Kitty Kitty Hello Kitty'
->>> import re
->>> re.findall('(?:Hello ){2}(?:Kitty ){2}', y)
-['Hello Hello Kitty Kitty ']
->>> 
